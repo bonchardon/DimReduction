92854971596573ab21477086e3c6659cb98fa849
bonchardon <61079942+bonchardon@users.noreply.github.com>
2021-12-16 17:37:05 +0200

    Update dimensionality_reduce.py

--- src/dimensionality_reduce.py
+++ src/dimensionality_reduce.py
@@ -29,91 +29,11 @@ from tensorflow.keras.layers import Conv2D, MaxPooling2D, UpSampling2D, Dropout,
 from scipy import stats
 import seaborn as sns
 
-import sys
+from DimReduction.src.pre_stage import PreProcessing, Vectorization
+from DimReduction.src.distance_metrics import DistanceMetrics
 
-sys.path.insert(1, "C:\/Users\Марина\Desktop\python_project")
-import pre_stage
-
-
-
-class PreProcessing:
-
-    def __init__(self):
-        pass
-
-    def txt_preprocess(self, file_link):
-        example_text = open(file_link, 'r',
-                            encoding='utf-8-sig').read()
-        example_text = example_text.split(' ')
-        print(len(example_text))
-
-        lemmatizer = WordNetLemmatizer()
-        stop_words = stopwords.words()
-        stop_words.extend(['th', 'asd', 'alphabet\/nalabama'])
-        words = [word for word in example_text if not word in stop_words]
-
-        lemmatized_words = [lemmatizer.lemmatize(w) for w in words]
-        print(len(lemmatized_words))
-
-        return lemmatized_words
-
-    def vec_count(self, cleaned_words):
-
-        """
-
-
-        :param cleaned_words:
-        :return:
-        """
-
-        vectorizer = CountVectorizer(binary=False, min_df=2)
-        X = vectorizer.fit_transform(cleaned_words)
-        feature_names = vectorizer.get_feature_names()
-        # X = X.toarray()
-
-        return X
-
-    def vec_TF_IDF(self, cleaned_words):
-
-        """
-        Applying Tf-Idf vectorizer for features extraction and getting outputs as scientific notation.
-        Note an issue: all the values, vectors are the same, so check this issue.
-
-        :param cleaned_words: preprocessed and cleaned data
-
-            TfIdfVectorizer parameters used:
-
-            :param ngram_range:     The lower and upper boundary of the range of n-values for different n-grams to be extracted.
-                                All values of n such that min_n <= n <= max_n will be used.
-                                For example an ngram_range of (1, 1) means only unigrams, (1, 2) means unigrams and bigrams, and (2, 2) means only bigrams.
-            :param max_df:          When building the vocabulary ignore terms that have a document frequency strictly higher than the given threshold (corpus-specific stop words).
-                                If float in range [0.0, 1.0], the parameter represents a proportion of documents, integer absolute counts.
-            :param min_df:          When building the vocabulary ignore terms that have a document frequency strictly lower
-                                than the given threshold.
-                                This value is also called cut-off in the literature.
-                                If float in range of [0.0, 1.0], the parameter represents a proportion of documents, integer absolute counts.
-                                This parameter is ignored if vocabulary is not None.
-        """
-
-        vectorizer = TfidfVectorizer(min_df=3, max_df=0.5, ngram_range=(1, 2))
-        X = vectorizer.fit_transform(cleaned_words)
-
-        #
-        pd.set_option('display.float_format', lambda x: '%.e2' % x)
-        df = pd.DataFrame(
-                            X.todense(),
-                            columns=vectorizer.get_feature_names()
-                            )
-
-        return df
-
-    def vec_hash(self, cleaned_words):
-        vectorizer = HashingVectorizer()
-        X = vectorizer.fit_transform(cleaned_words)
-        return X
-
-    def vec_bow(self, cleaned_words):
-        pass
+pre = PreProcessing()
+vec = Vectorization()
 
 
 class DimensionRed:
@@ -273,59 +193,18 @@ class Features:
         autoencoder(data=txt)
 
 
-class DistanceMetrics:
-
-    def __init__(self):
-        pass
-
-    def euclid_dist(self, dataframe):
-
-        # one of the possible alternatives
-        distance_matrix = euclidean_distances(dataframe)
-
-        dist = DistanceMetric.get_metric('euclidean')
-        values = dist.pairwise(dataframe)
-
-        X_euclid = pd.DataFrame(values, index=dataframe.index)
-
-        first_100_words = X_euclid.sort_values(by=[0, 1, 2, 3, 4, 5, 6], ascending=False).index[0:100]
-        return first_100_words
-
-    def cosine_dist(self, dataframe):
-        cosine_distance = cosine_similarity(dataframe)
-        X_cosine_dist = pd.DataFrame(cosine_distance, index=dataframe.index)
-
-        X_cosine_dist = X_cosine_dist[[0, 1, 2, 3, 4, 5, 6]]
-        first_100_words_cosine = X_cosine_dist.sort_values(by=[0, 1, 2, 3, 4, 5, 6], ascending=False).index[0:100]
-        return first_100_words_cosine
-
-    def manhattan_dist(self, dataframe):
-        dist_manhattan = DistanceMetric.get_metric('manhattan')
-        values_manhattan = dist_manhattan.pairwise(dataframe)
-
-        X_manhattan = pd.DataFrame(values_manhattan, index=dataframe.index)
-
-        X_manhat_dist = X_manhattan[[0, 1, 2, 3, 4, 5, 6]]
-
-        first_100_words_manhattan = X_manhat_dist.sort_values(by=[0, 1, 2, 3, 4, 5, 6],
-                                                              ascending=False).index[0:100]
-        return first_100_words_manhattan
-
-
 if __name__ == "__main__":
 
     """
     In NLP, the aspect of word vectors (as numerical representation of words in corpora/dataset) and dimensionality reduction
     are some of the most important. 
     """
+    pre = PreProcessing()
+    vec = Vectorization()
 
-    preprocessing = PreProcessing()
     reducing = DimensionRed()
+    
     distance = DistanceMetrics()
-
-    print(preprocessing.vec_TF_IDF(cleaned_words=preprocessing.txt_preprocess(file_link='wikitext1.txt')))
-
-
 
 
     """
